913ca10b4e92abc90d61b2f75aea3c8ddc0e03f8
eleana1232 <160231160+eleana1232@users.noreply.github.com>
2025-03-24 22:17:42 +0200

    Add files via upload
    This project is still in proccess

---
+++ blocks.py
@@ -0,0 +1,67 @@
+from block import Block
+from position import Position 
+
+class LBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0,2), Position(1,0), Position(1,1), Position(1,2)],
+            1: [Position(0,1), Position(1,1), Position(2,1), Position(2,2)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
+            3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)]
+        }
+
+class JBlock(Block):
+    def __init__(self):
+        super().__init__(id = 2)
+        self.cells = {
+            0:[Position(0,0), Position(1,0), Position(1,1), Position(1,2)],
+            1:[Position(0,1), Position(0,2), Position(1,1), Position(2,1)],
+            2:[Position(1,0), Position(1,1), Position(1,2), Position(2,2)],
+            3:[Position(0,1), Position(1,1), Position(2,0), Position(2,1)]
+        }
+
+class IBlock(Block):
+    def __init__(self):
+        super().__init__(id = 2)
+        self.cells = {
+            0:[Position(0,0) ,Position(1,0), Position(1,1), Position(1,2)],
+            1:[Position(0,1), Position(0,2), Position(1,1), Position(2,1)],
+            2:[Position(1,0), Position(1,1), Position(1,2), Position(2,2)],
+            3:[Position(0,1), Position(1,1), Position(2,0), Position(2,1)]
+        }
+
+class OBlock(Block):
+    def __init__(self):
+        super().__init__(id = 4)
+        self.cells = {
+            0:[Position(0,0), Position(0,1), Position(1,0), Position(1,1)],
+            1:[Position(0,0), Position(0,1), Position(1,0), Position(1,1)],
+            2:[Position(0,0), Position(0,1), Position(1,0), Position(1,1)], # type: ignore
+            3:[Position(0,0), Position(0,1), Position(1,0), Position(1,1)] # type: ignore
+        }
+
+
+class SBlock(Block):
+    def __init__(self):
+        super().__init__(id = 5)
+        self.cells = {
+            0:[Position(0,1), Position(0,2), Position(1,0), Position(1,1)],
+            1:[Position(0,1), Position(1,1), Position(1,2), Position(2,2)],
+            2:[Position(1,1), Position(1,2), Position(2,0), Position(2,1)],
+            3:[Position(0,0), Position(1,0), Position(1,1), Position(2,1)]
+        }
+
+class TBlock(Block):
+    def __init__(self):
+        super().__init__(id = 6)
+        self.cells = {
+            0:[Position(0,1), Position(1,0), Position(1,1), Position(1,2)],
+            1:[Position(0,1), Position(1,1), Position(1,2), Position(2,1)],
+            2:[Position(1,0), Position(1,1), Position(1,2), Position(2,1)],
+            3:[Position(0,1), Position(1,0), Position(1,1), Position(2,1)]
+        }
+
+
+
+        
---
+++ tetris.game.py
@@ -0,0 +1,32 @@
+import pygame,sys
+from grid import Grid
+from blocks import *
+
+pygame.init()
+dark_blue = (44,44,127)
+
+screen = pygame.display.set_mode((300,600))
+pygame.display.set_caption("Python Tetris")
+
+clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+block = _Block()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+    block.draw(screen)
+   
+    pygame.display.update()
+    clock.tick(60)
+
+  
